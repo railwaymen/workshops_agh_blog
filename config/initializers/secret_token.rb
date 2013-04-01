8dc3da8765109e13bce398fc721d55d95550d378
Tomek Wielgocki <tomek@railwaymen.org>
2013-04-01 14:09:07 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WorkshopsAghBlog::Application.config.secret_token = '7478933eadc1a7cec15a286e8fc635984304a94624b9acebf4cec8c62aa4521a3dc0fcfe2804363e71bd9eb6ed7f8209ec2ca59f2601c04d42ab3c8dd5a1af5c'
