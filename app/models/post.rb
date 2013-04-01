6dde70532ee80366ba5ba4e94f5b29ae9e93dbb8
Tomek Wielgocki <tomek@railwaymen.org>
2013-04-01 17:38:46 +0200

    After chapter 7

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
   has_many :comments
-  attr_accessible :title, :content
+  attr_accessible :title, :content, :image
+  has_attached_file :image, styles: { medium: '400x400>' }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130401145540) do
+ActiveRecord::Schema.define(:version => 20130401152027) do
 
   create_table "comments", :force => true do |t|
     t.text     "content"
@@ -24,8 +24,12 @@ ActiveRecord::Schema.define(:version => 20130401145540) do
   create_table "posts", :force => true do |t|
     t.string   "title"
     t.text     "content"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", :force => true do |t|
